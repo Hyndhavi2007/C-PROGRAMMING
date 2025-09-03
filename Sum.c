add2f463ca9045a64d0333ddd15aa3950821c602
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2025-09-03 21:32:28 +0530

    Add Sum.c for calculating the sum of two numbers

---
+++ Sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+    int sum;
+    printf("Enter the first number a :");
+    scanf("%d", &a);
+    printf("Enter the second number b : ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The Sum of Two Number is %d\n", sum);
+    return 0;
+}
