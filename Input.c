23c8f7193ec78c51d1a2dca6231ed92c7aee4428
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2025-09-03 21:28:22 +0530

    Add initial C program files for age input and output

---
+++ Input.c
@@ -0,0 +1,8 @@
+  #include<stdio.h>
+  int main(){
+  int age;
+  printf("Enter your age: ");
+  scanf("%d",&age);//& gives address of variable.{ & → Ampersand}
+  printf("your age is %d\n", age);
+  return 0;
+  }
---
+++ Output.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+int age = 22;
+printf("age is %d",age); // %d is the placeholder for integer.
+
+return 0;
+}
